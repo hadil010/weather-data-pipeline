bb39224bc3af79e0c4ff82e84744c95104ccb75c
hadil010 <130673320+hadil010@users.noreply.github.com>
2025-11-22 21:30:29 -0700

    Add setup_project script

---
+++ src/setup_project.py
@@ -0,0 +1,34 @@
+import os
+import datetime from datetime 
+
+def create_folders():
+  folders = [
+             "data",
+            "data/bronze",
+            "data/silver",
+            "data/gold",
+            "data/archive",
+            "logs",
+            "config",
+            "src",
+            "database"]
+
+  for folder in folders:
+    os.makedirs(folder,exist_ok=True)
+    print(f"[ok] folder ready: {folder}")
+    
+def show_structure():
+    for root, dirs, files in os.walk("."):
+        print(root)
+        for d in dirs:
+            print(f"  ┗ {d}/")
+def main():
+    print("Starting project setup...")
+    print(f"Time: {datetime.now()}")
+    create_folders()
+    print("\nProject structure:")
+    show_structure()
+    print("\nSetup completed successfully.")
+if __name__ == "__main__":
+    main()
+    
